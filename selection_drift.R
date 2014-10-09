2fb4045a69b234c9388b2f3fdd777a120e567dac
Diogo Meyer <diogo@ib.usp.br>
2014-10-09 12:03:14 -0300

    Updates
    Included line to count number of times the favored allele was fixed at the end of the simulation

--- selection_drift.R
+++ selection_drift.R
@@ -1,6 +1,6 @@
 ## set parameters
 # population size
-N <- 1000
+N <- 100
 
 # generations
 ngen <- 2000
@@ -9,14 +9,17 @@ ngen <- 2000
 s <- 0.05 
 
 # initial allele frequency
-f <- 0.01
+f <- 0.1
+
+# initializing value of the number of fixed populations
+fix <- 0
 
 #####
 
 # Open plot device
 plot(0, xlim=c(0, ngen), ylim=c(0, 1), type="n",
      xlab="generation", ylab="frequency", 
-     main=paste("Increasing in allele frequency over", ngen, "generations"))
+     main=paste("Allele frequency change over", ngen, "generations"))
 
 ## do the calculations
 for(i in 1:20) { 
@@ -30,15 +33,20 @@ for(i in 1:20) {
   # for each subsequent generation
   for(j in 2:length(p)) { 
     
-    # calculate allele frequency
+    # calculate allele frequency after selection
     af <- p[j-1] / (1 - (s*(1-p[j-1])^2))
 
-    # make population based on new allele freq
-    pop <- rep(0:1, times=round(c(af, 1-af)*N))  
+    # make population based on these allele frequencies
+    pop <- rep(0:1, times=round(c(af, 1-af)*N))
+    
+    # sample with replacement to simulate drift
     pop <- sample(pop, replace=TRUE) 
 
     # take the frequency of allele "0"
     p[j] <- mean(pop == 0) 
+    
+    # count number of fixations
+    if(p[ngen]==1){fix <- fix+1}
   } 
   
   # plot the values for population i
@@ -56,5 +64,6 @@ for(i in 2:length(pd)) {
 # plot
 lines(pd, lwd=3)
 legend(x=1500, y=0.2, 
-       legend=c("simulated populations", "deterministic curve"),
+       legend=c("simulated", "deterministic"),
        lty=c(2,1), lwd=c(1,3), col=c("grey60","black"), bty="n")
+       text(1800, 0.3, paste("fixed=",fix))
