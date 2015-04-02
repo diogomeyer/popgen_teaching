9a457516002aa20826272dc87b5f92200243fa04
Diogo Meyer <diogo@ib.usp.br>
2015-04-02 15:11:12 -0300

    Update selection_deterministic.R
    Cleaned up code for lines, tested functions vs Populus

--- selection_deterministic.R
+++ selection_deterministic.R
@@ -13,15 +13,15 @@ p.rec <- numeric()
 p.cod[1] <- 0.001 
 p.dom[1] <- 0.001
 p.rec[1] <- 0.001
-for(i in 2:850)
+for(i in 2:1000)
     { 
         p.cod[i] <- delta.p(p.cod[i-1], 0.02, 0.5)
         p.dom[i] <- delta.p(p.dom[i-1], 0.02, 0)
         p.rec[i] <- delta.p(p.rec[i-1], 0.02, 1)
     }
-plot(p.cod, type="lines", col="red", xlab="Generation", ylab="Allele frequency (p)")              
-lines(p.dom, type="lines", col="black")
-lines(p.rec, type="lines", col="blue")
+plot(p.cod, type="l", col="red", xlab="Generation", ylab="Allele frequency (p)")              
+lines(p.dom, type="l", col="black")
+lines(p.rec, type="l", col="blue")
 
 
 
