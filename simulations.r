40bc028dcb976b2150c012e02866b25e2c72284d
bbitarello <barbara@ib.usp.br>
2015-04-07 00:52:25 -0300

    script in R to read in the sample_stats outputs

--- simulations.r
+++ simulations.r
@@ -6,4 +6,13 @@
 
 #Read in files
 
-read.table (
+path.1<- '/Users/barbarabitarello/bio5789/'   #replace with the path for where YOUR files are
+
+#Copy and paste the entire block below to read in the sample_stats outputs#
+
+read.table (paste0(path.1,'pop.t0.01.stats.out'))->pop.t0.01.stats
+read.table (paste0(path.1,'pop.t0.05.stats.out'))->pop.t0.05.stats
+read.table (paste0(path.1,'pop.t0.12.stats.out'))->pop.t0.12.stats
+read.table (paste0(path.1,'pop.t0.5.stats.out'))->pop.t0.5.stats
+read.table (paste0(path.1,'pop.t1.stats.out'))->pop.t1.stats
+read.table (paste0(path.1,'pop.t4.stats.out'))->pop.t4.stats
