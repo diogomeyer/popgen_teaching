322d98ab8198c38a57f03d775d97a45a2f3bab97
bbitarello <barbara@ib.usp.br>
2015-04-07 01:40:12 -0300

    almost done

--- simulations.r
+++ simulations.r
@@ -8,11 +8,68 @@
 
 path.1<- '/Users/barbarabitarello/bio5789/'   #replace with the path for where YOUR files are
 
+#BLOCK 1: read in the data
 #Copy and paste the entire block below to read in the sample_stats outputs#
+read.table(paste0(path.1,'pop.t0.01.stats.out'))->pop.t0.01.stats   #reads in this file 
+colnames(pop.t0.01.stats)<-c('pi', 'ss', 'D')  #adds names to collumns
+read.table(paste0(path.1,'pop.t0.05.stats.out'))->pop.t0.05.stats
+colnames(pop.t0.05.stats)<-c('pi', 'ss', 'D')
+read.table(paste0(path.1,'pop.t0.12.stats.out'))->pop.t0.12.stats
+colnames(pop.t0.12.stats)<-c('pi', 'ss', 'D')
+read.table(paste0(path.1,'pop.t0.5.stats.out'))->pop.t0.5.stats
+colnames(pop.t0.5.stats)<-c('pi', 'ss', 'D')
+read.table(paste0(path.1,'pop.t1.stats.out'))->pop.t1.stats
+colnames(pop.t1.stats)<-c('pi', 'ss', 'D')
+read.table(paste0(path.1,'pop.t4.stats.out'))->pop.t4.stats
+colnames(pop.t4.stats)<-c('pi', 'ss', 'D')
+read.table(paste0(path.1,'pop.const.stats.out'))->pop.const.stats
+colnames(pop.const.stats)<-c('pi', 'ss', 'D')
+###End of Block#################################
+################################################
 
-read.table (paste0(path.1,'pop.t0.01.stats.out'))->pop.t0.01.stats
-read.table (paste0(path.1,'pop.t0.05.stats.out'))->pop.t0.05.stats
-read.table (paste0(path.1,'pop.t0.12.stats.out'))->pop.t0.12.stats
-read.table (paste0(path.1,'pop.t0.5.stats.out'))->pop.t0.5.stats
-read.table (paste0(path.1,'pop.t1.stats.out'))->pop.t1.stats
-read.table (paste0(path.1,'pop.t4.stats.out'))->pop.t4.stats
+#BLOCK 2: summarize ##############################
+#run
+apply(pop.const.stats, 2, mean)  #mean of pi, SS and D for the simulations with constant N
+
+#Now, run this enormous command to generate one data.frame with all simualtion results
+
+all.stats.sum<-rbind(
+apply(pop.const.stats, 2, mean),
+apply(pop.t0.01.stats, 2, mean),
+apply(pop.t0.05.stats,2,mean),
+apply(pop.t0.12.stats,2,mean),
+apply(pop.t0.5.stats,2, mean),
+apply(pop.t1.stats,2, mean),
+apply(pop.t4.stats,2,mean)
+)
+all.stats.sum  #check the object
+#add rownames
+rownames(all.stats.sum)<-c('t.const','t0.01','t0.05','t0.12','t0.5','t1','t4')
+############END OF BLOCK ###################
+############################################
+
+
+###PLOTS #############
+
+#Run this to check Pi, SS and TajD for the constant pop size simulations.
+##
+par(mfrow=c(3,1)) #this allows the plots to be in 3x1 (row, col) configuration.
+sapply(1:3, function(x) hist(pop.const.stats[,x], main='Constant pop size', 
+xlab=colnames(pop.const.stats)[x], nclass=100, col='cornflowerblue'))  #this plots pi, SS, and D, sequentially, on the same page.
+
+#check again where the means of these histograms fall
+all.stats.sum #check first line
+
+#Now, let's make one big pdf file with all of these plots, for all simulations.
+#######Block 3: Plot #####################
+##########################################
+pdf('all.stats.plots.pdf')
+par(mfrow=c(3,1))
+sapply(1:3, function(x) hist(pop.const.stats[,x], main='Constant pop size', 
+xlab=colnames(pop.const.stats)[x], nclass=100, col='cornflowerblue')) #histogram is not super appropriate for SS (discrete)
+#
+sapply(1:3, function(x) hist(pop.t0.01.stats[,x], main='Expansion at t=0.01', 
+xlab=colnames(pop.t0.01.stats)[x], nclass=100, col='cornflowerblue'))  
+#
+sapply(1:3, function(x) hist(pop.t0.05.stats[,x], main='Expansion at t=0.05', 
+xlab=colnames(pop.t0.05.stats)[x], nclass=20, col='cornflowerblue'))
