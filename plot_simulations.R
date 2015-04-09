27d11e160c4a46890d1b70a35fcca2b5263b4e62
VitorAguiar <vitor.aguiar@me.com>
2015-04-09 10:45:35 -0300

    exclude first plot option

--- plot_simulations.R
+++ plot_simulations.R
@@ -14,35 +14,9 @@ times <- gsub("^.*t(.*)\\.stats\\.out$", "\\1", files)
 times[times==""] <- 0
 names(all_files) <- paste0("t", times)
   
-# Plot option 1 -------------------------------------------------------------- #
-
-# Open plot device (pdf)
-pdf(paste0("all_plots.pdf"), height = 12)
-
-# Set how plots are arrange in plot window (given odd or even number of plots)
-if (length(all_files) %% 2 == 0) { 
-  layout(matrix(1:length(all_files), ncol = 2, byrow = TRUE))
-} else {
-  layout(matrix(c(1:length(all_files), 0), ncol = 2, byrow = TRUE))
-}
-
-# plot a given variable in 'vars' and plot it for all files (different times)
-for(var in vars) {
-  
-  sapply(seq_along(all_files), function(x) {
-	 hist(all_files[[x]][var][[1]], 
-	      main = paste(var, sprintf("t = %s", times[x])), 
-	      xlab = colnames(all_files[[x]])[var], col = 'cornflowerblue')
-  })
-}
-
-# close pdf device
-dev.off()
-
-# Plot option 2 -------------------------------------------------------------- #
-
+# Plot
 # We're going to use 3 packages: magrittr, dplyr, ggplot2.
-# If you don't those packages installed, first execute the follow line:
+# If you don't those packages installed, first execute the following line:
 
 # install.packages(c("magrittr", "dplyr", "ggplot2"))
 
