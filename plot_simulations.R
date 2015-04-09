a8e33dbba20c2203a2e15207bbb0f8bce1b0c8b8
VitorAguiar <vitor.aguiar@me.com>
2015-04-09 10:03:15 -0300

    update way multiple ggplots are saved to device

--- plot_simulations.R
+++ plot_simulations.R
@@ -8,22 +8,20 @@ times[times==""] <- 0
 names(all_files) <- paste0("t", times)
   
 # Option 1
-pdf(paste0("all.times.pdf"), height = 12)
+pdf(paste0("all_plots.pdf"), height = 12)
 
-if (length(all_files)%%2 == 0) { 
-  layout_mat <- matrix(1:length(all_files), ncol = 2, byrow = TRUE)
+if (length(all_files) %% 2 == 0) { 
+  layout(matrix(1:length(all_files), ncol = 2, byrow = TRUE))
 } else {
-  layout_mat <- matrix(c(1:length(all_files), 0), ncol = 2, byrow = TRUE)
+  layout(matrix(c(1:length(all_files), 0), ncol = 2, byrow = TRUE))
 }
 
-layout(layout_mat)
 for(var in vars) {
-
+  
   sapply(seq_along(all_files), function(x) {
 	 hist(all_files[[x]][var][[1]], 
-	      main=paste(var, sprintf("t = %s", times[x])), 
-	      xlab=colnames(all_files[[x]])[var], 
-	      col='cornflowerblue')
+	      main = paste(var, sprintf("t = %s", times[x])), 
+	      xlab = colnames(all_files[[x]])[var], col = 'cornflowerblue')
   })
 }
 
@@ -39,10 +37,14 @@ for (i in seq_along(all_files))
 
 all_files_df <- rbind_all(all_files)
 
-plot_ms <- function(var) {
-  ggplot(all_files_df, aes_string(var)) + 
-	 geom_histogram() +
-	 facet_wrap(~time, scales = "free")
-}
+plot_list <- vector("list", length(vars))
+names(plot_list) <- vars
 
-plot_ms("pi")
+for (var in vars) 
+  plot_list[[var]] <- ggplot(all_files_df, aes_string(var)) + 
+                    geom_histogram() + 
+		    facet_wrap(~time, scales = "free")
+
+pdf("all_plots.pdf")
+plot_list
+dev.off()
