a8477e4ec4957577225e3582ac743ac55e3a78f5
VitorAguiar <vitor.aguiar@me.com>
2015-04-09 19:55:08 -0300

    change obj name vars to variable_names

--- plot_simulations.R
+++ plot_simulations.R
@@ -1,12 +1,12 @@
 # Variables names:
-vars <- c("pi", "ss", "D")
+variable_names <- c("pi", "ss", "D")
 
 # get all file names starting with 'pop.t' in your working directory,
 # assuming that the output files from the simulations are in your wd:
 files <- list.files(pattern = "pop\\.t*")
 
 # read all files in a list
-all_files <- lapply(files, read.table, col.names = vars)
+all_files <- lapply(files, read.table, col.names = variable_names)
 
 # get the 'times' from the file names
 times <- gsub("^.*t(.*)\\.stats\\.out$", "\\1", files)
@@ -37,11 +37,11 @@ for (i in seq_along(all_files))
 all_files_df <- rbind_all(all_files)
 
 # create a list of plots
-plot_list <- vector("list", length(vars))
-names(plot_list) <- vars
+plot_list <- vector("list", length(variable_names))
+names(plot_list) <- variable_names
 
 # make a plot for each variable and store as an element of plot_list
-for (var in vars) 
+for (var in variable_names) 
   plot_list[[var]] <- ggplot(all_files_df, aes_string(var)) + 
                     geom_histogram(aes(fill = time)) + 
 		    facet_wrap(~ time, ncol = 1)
